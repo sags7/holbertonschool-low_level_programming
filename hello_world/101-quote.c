1a8a692d8361c029eb618e9dd26db7b6326c4aed
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 16:59:58 -0500

    removed strlen func to pass test

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
 *main- the programs entry point
 *Return: always returns 0
 */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(STDOUT_FILENO, message, strlen(message));
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, message, 60);
 return (0);
 }
