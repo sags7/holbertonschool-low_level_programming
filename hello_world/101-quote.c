2711792ff314381eeec121818f20c296ed43b768
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 17:07:31 -0500

    chaged output from std out to std err

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main- the programs entry point
+*main- the program writes a message to the std error
 *Return: always returns 0
 */
 int main(void)
 {
 char message[] =
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO, message, 59);
+write(STDERR_FILENO, message, 59);
 return (0);
 }
