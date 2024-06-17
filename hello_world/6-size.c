2de8a022aab7f7a9045468c2d2a0df36d78ddd5b
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 15:20:02 -0500

    add line breaks to the message and change type of variable in string to ul

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)", sizeof(char));
-printf("Size of an int: %c byte(s)", sizeof(int));
-printf("Size of a long int: %c byte(s)", sizeof(long));
-printf("Size of a long long int: %c bytes(s)", sizeof(long double));
-printf("Size of a float: %c byte(s)", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long double));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
