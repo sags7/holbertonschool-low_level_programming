c8da4a57c8bb3d30684ab9086c94043634fe6a4a
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 14:59:01 -0500

    add program that prints a hardcoded message

---
+++ hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- program's entry point
+ * Return: program always retruns 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
