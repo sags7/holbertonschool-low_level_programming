730fd9626e78c89be2f060f6edebe28abd6072fb
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 14:30:30 -0500

    added commented description of the main function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/*
+ *puts a hardcoded string into the standard output
+ *then retruns with 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
