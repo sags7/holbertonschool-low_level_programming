33f2ffe8df653d68a5a624655caad4c0690d919e
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 17:05:00 -0500

    broke up longer than 81 characters line

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,7 +6,8 @@
 */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDOUT_FILENO, message, 59);
 return (0);
 }
