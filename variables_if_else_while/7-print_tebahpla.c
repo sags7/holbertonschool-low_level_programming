72a82a5b5de8844b8ff884618f6fe1e43383262d
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-18 13:15:48 -0500

    add scirpt that prints alphabet baclwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Main program entry point
+ * Return: always returns 0;
+*/
+int main(void)
+{
+	char alphabet[] = "zyxwvutsrqponmlkjihgfedcba";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
