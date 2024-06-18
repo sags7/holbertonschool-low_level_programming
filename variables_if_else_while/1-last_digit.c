19c45ca5e628422f51a98c281d3aec8f0357f50f
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 21:43:07 -0500

    added lastDigit variable to simplify things and added code for negative numbers

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,13 +9,15 @@
 int main(void)
 {
 int n;
+int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n % 10 > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-else if (n % 10 == 0)
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
-else if (n % 10 < 6 && n % 10 != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+n < 0 ? lastDigit = -(n % 10) : n % 10;
+if (lastDigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+else if (lastDigit == 0)
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+else if (lastDigit < 6 && lastDigit != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 return (0);
 }
