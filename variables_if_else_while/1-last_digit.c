194f5522eaed9f9eedc376d654e5560b4add595e
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-17 21:54:27 -0500

    removed ternary operator

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int n;
 int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-n < 0 ? lastDigit = -(n % 10) : n % 10;
+lastDigit = n % 10;
 if (lastDigit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 else if (lastDigit == 0)
