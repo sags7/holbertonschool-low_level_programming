2d647d34526df4cb9a64bfa7705b29ee111174d4
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-19 10:35:23 -0500

    add comb4 exercise

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,19 +10,25 @@ int main(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if (b <= a)
-				continue;
-			putchar('0' + a);
-			putchar('0' + b);
-			if (!((a == 9) && (b == 9)))
+			for (c = 0; c < 10; c++)
 			{
-				putchar(',');
-				putchar(' ');
+				if (c <= b || b <= a)
+					continue;
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar('0' + c);
+
+				if (a != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
