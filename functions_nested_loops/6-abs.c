2363cd2728481b0a0b1a985d60b82ce1461ad5fa
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-20 10:32:14 -0500

    added _abs() that returns the absolute value of an int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * _abs- returns the absolute value of an int
+ *
+ * @i: the integer to be tested
+ *
+ * Return: the absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		return (i * -1);
+	else
+		return (i);
+}
