3ab1024f2ab7fd2f5ad9bd7632540cbf7ba1deab
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-20 09:40:39 -0500

    added betty standard documentation for -islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,3 +1,11 @@
+/**
+ * _islower- determines if an int represents a lowercase character in ASCII
+ *
+ *@c: the int value of an ASCII character
+ *
+ *Return: 1 if its lowercase 0 if its anything else
+ */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
