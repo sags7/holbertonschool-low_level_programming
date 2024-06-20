774b97d5ed23f1d57ed23cced1e97d4d51434cb0
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-20 17:57:08 -0500

    created add() that adds two integers together and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add- adds two integers together and returns the value
+ *
+ * @a: first integer to add
+ * @b: second integer to add
+ *
+ * Return: the addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
