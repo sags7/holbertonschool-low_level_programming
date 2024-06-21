a93dc6ffbc0162394a90f36c9a1a1f5bb6a20653
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-21 16:55:04 -0500

    chage from long long int to unsigned long int to support c89

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -5,18 +5,19 @@
  *  Return: always 0 if successfully executed
  */
 
-void main(void)
+int main(void)
 {
 	int i;
-	long long int a = 1, b = 2, r;
+	unsigned long int a = 1, b = 2, r;
 
-	printf("%lld\n", a);
-	printf("%lld\n", b);
+	printf("%lu\n", a);
+	printf("%lu\n", b);
 	for (i = 0; i < 47; i++)
 	{
 		r = a + b;
 		a = b;
 		b = r;
-		printf("%lld\n", r);
+		printf("%lu\n", r);
 	}
+	return (0);
 }
