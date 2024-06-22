7c24bc504ccb69b5752ebf7a7f7429ebf6ec810e
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-21 20:00:34 -0500

    add Even Liber Abbaci exercise

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- program entry point
+ *
+ *  Return: always 0 if successfully executed
+ */
+
+int main(void)
+{
+	int i;
+
+	unsigned long int a = 1, b = 2, r, sum = 2;
+
+
+	for (i = 0; i < 100; i++)
+	{
+		r = a + b;
+		a = b;
+		b = r;
+
+		if (r > 4000000)
+			break;
+
+		if (r % 2 == 0)
+			sum += r;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
