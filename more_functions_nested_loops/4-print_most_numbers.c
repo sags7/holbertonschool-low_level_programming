70f6a4f0b034cf325a838ecda51916e495eb551a
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-24 13:15:15 -0500

    fixed betty documentation style

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers- prints the numbers from 0 to 9, excluing 4 and 2
+ * print_most_numbers- prints the numbers from 0 to 9, excluing 4 and 2
  *
  */
 void print_most_numbers(void)
