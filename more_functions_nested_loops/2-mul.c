93df4a0d8779aae88b854426fa58ba3f6e688b0b
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-24 12:47:04 -0500

    add mul() that retruns the multiplication of two ints

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul- multiplies two ints and retruns the result
+ *
+ * @a: first int to multiply
+ * @b: second int to multiply
+ * Return- returns the result of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
