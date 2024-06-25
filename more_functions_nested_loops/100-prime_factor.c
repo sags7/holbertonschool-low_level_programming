6d21a97999c59f1615d2b4cafef79211bb908251
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-25 12:09:29 -0500

    add program that prints the largest prime factor of 612852475143

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
+#include <stdio.h>
+/**
+ * isPrime- returns 1 if n is prime, 0 if not
+ * @n: the number to check if its prime
+ * Return: returns 1 if n is prime, 0 if not
+ */
+int isPrime(int n)
+{
+	int i;
+
+	if (n <= 1)
+		return (0);
+	for (i = 2; i <= n; i++)
+		if (n % i == 0 && i != n)
+			return (0);
+	return (1);
+}
 /**
  * main- program entry point. determines and prints the largest prime factor
  * of 612852475143 followed by an new line
+ * Return: always 0
  */
 int main(void)
 {
-	int value = 612852475143, factor = 0, x =2;
-	bool found = false;
+	long int val = 612852475143, highest_fac = 0, fac;
 
-	while (!found)
+	for (fac = 2; fac <= val; fac += 2)
 	{
-		if(value % x == 0)
+		if (fac == 2)
+			fac--;
+		if (isPrime(fac) == 1 && val % fac == 0)
 		{
-
+			val /= fac;
+			if (fac > highest_fac)
+				highest_fac = fac;
 		}
-	    x++;
 	}
+	printf("%ld\n", highest_fac);
 }
