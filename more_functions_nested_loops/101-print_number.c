c6ee1dcdf74137942f63a71872f68ef2a47d0be6
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-25 09:13:05 -0500

    changed the script to use recursiveness instead of the pow function

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,5 @@
 #include "main.h"
 /**
- * _pow- returns x to the power of exp
- * @x: the number to exponentiate
- * @exp: the exponent of x
- * Return: returns x to the power of exp
- */
-unsigned int _pow(unsigned int x, unsigned int exp)
-{
-	unsigned int n, res;
-
-	if (exp == 0)
-		return (1);
-	else if (exp == 1)
-		return (x);
-
-	res = x;
-
-	for (n = 2; n <= exp; n++)
-	{
-		res *= x;
-	}
-	return (res);
-}
-
-/**
  * print_number- prints the input int using only _putchar()
  * @n: the number to print out
  */
