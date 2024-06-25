2cc55ae1a040d6946a4bfd9e0de39eebb75dbc5e
sebastian aramburo <juansebastianaramburo@gmail.com>
2024-06-24 19:31:25 -0500

    added print_number() tha prints an int using only _putchar();

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- program entry point. determines and prints the largest prime factor
+ * of 612852475143 followed by an new line
+ */
+int main(void)
+{
+	int value = 612852475143, factor = 0, x =2;
+	bool found = false;
+
+	while (!found)
+	{
+		if(value % x == 0)
+		{
+
+		}
+	    x++;
+	}
+}
