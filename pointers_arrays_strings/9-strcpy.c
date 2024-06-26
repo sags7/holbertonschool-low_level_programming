ed3434a9ffbd2ed8f7ad75a683e8861eda41e29f
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-26 16:33:23 -0500

    changed condition to check for 0  instead of null operator

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != 0)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
 	return (dest);
