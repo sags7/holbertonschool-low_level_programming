41f6b5019996958b9590881952c0790d951f83c4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-26 12:53:57 -0500

    added _strlen the 5-rev_sring.c file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * _strlen- returns the length of a string
+ *
+ * @s: the starting memory position of the string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int retVal = 0;
+
+	while (*(s + retVal) != '\0')
+		retVal++;
+	return (retVal);
+}
+/**
  * rev_string- reverses the string stored in address *s
  * @s: The location of the first character of the string
  */
