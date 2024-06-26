ac9869eee5daae50a211641a36ddf83f7289ad33
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-26 16:02:06 -0500

    added _strcpy() that copies a string from src to dest

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy- copies the string pointed to by src including the \0
+ *  to the buffer pointed at by dest
+ * @dest: the destination pointer
+ * @src: the source pointer
+ * Return: returns the the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
