aacc1c8011273ee898b95221e0650f182649ff15
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-26 13:16:48 -0500

    added puts2() that prints every other letter of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2- prints every other letter of a string followed by a new line to stdout
+ * @str: the starting memory location of the string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if(i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
