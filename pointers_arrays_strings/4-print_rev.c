7d5b555d8446a20ea084624685bb34ffb7ec3ee3
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-25 23:24:09 -0500

    added print_rev() that prints a string in reverse, then a \n

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev_recursive- prints a string in reverse, followed by a new line
+ *@s: the starting memory position of the string to be printed in reverse
+ *@start: the memory location of the first letter of the string
+ */
+void print_rev_recursive(char *s, char *start)
+{
+	if (*s != '\0')
+		print_rev_recursive((s + 1), start);
+	_putchar(*s);
+
+	if (s == start)
+		_putchar('\n');
+}
+/**
+ * print_rev- prints a string to stdout in reverse, followed by a new line
+ *@s: the starting memory position of the string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	print_rev_recursive(s, s);
+}
