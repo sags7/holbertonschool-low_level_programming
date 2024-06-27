44ca29c8305d9e50451e8694bb212ee1877accb3
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-27 14:25:18 -0500

    added _strncat() that concatenates from a soruce up to nth elememt

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat- appends the src string up to the nth byte
+ *  to the dest string, overwritting its \0
+ * @dest: the string that will get src appended to
+ * @src: the string that'll get appended to dest
+ * @n: the number of chars to get from src
+ * Return: the memory address of the resulting dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, x = 0;
+
+	while (*(dest + i))
+		i++;
+	while (*(src + x) && x < n)
+	{
+		*(dest + i) = *(src + x);
+		i++;
+		x++;
+	}
+	return (dest);
+}
