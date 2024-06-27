d7fde85a9f1c4162dbdcde430a285457acdde012
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-27 18:55:02 -0500

    add cap_string() that capitalizes every first letter after a word separator

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * cap_string- changes every first letter of a word to uppercase
+ * @c: the string to be affected
+ * Return: returns the converted string
+ */
+char *cap_string(char *c)
+{
+	int i, b;
+	char seps[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; *(c + i); i++)
+	{
+	    if (*(c + i) >= 'a' && *(c + i) <= 'z')
+			for (b = 0; *(seps + b); b++)
+			{
+				if (*(c + i -1) == *(seps + b))
+					*(c + i) -= 32;
+			}
+	}
+	return (c);
+}
