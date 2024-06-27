3ba77d8c2f2d3e5bb828030fa0fbb725a2e75cf5
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-27 15:34:13 -0500

    added strcmp() that compares two strings and returns

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;;
+	int i = 0;
 
-	while ((*(s1 + i) == *(s2 +i)) && (*(s1 + i) != '\0' && *(s2 + i) != '\0'))
+	while ((*(s1 + i) == *(s2 + i)) && (*(s1 + i) != '\0' && *(s2 + i) != '\0'))
 		i++;
 	return (s1[i] - s2[i]);
 }
