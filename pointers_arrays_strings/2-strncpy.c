f7984c29f78cf8c66eec08c8bfa4f676457bf87a
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-27 14:34:52 -0500

    added _strncpy() that copies a string up to its nth element into a dest string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strncpy- copies the src string up to the nth byte
+ *  to the dest string, overwritting its \0
+ * @dest: the string that will get src appended to
+ * @src: the string that'll get appended to dest
+ * @n: the number of chars to get from src
+ * Return: the memory address of the resulting dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while  (x < n)
+	{
+		*(dest + x) = *(src + x);
+		x++;
+	}
+	return (dest);
+}
