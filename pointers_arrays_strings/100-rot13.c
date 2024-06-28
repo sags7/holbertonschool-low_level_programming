4ceed1d1cc7577652b4c35bfcd85d4f0e959f30d
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-06-27 21:24:44 -0500

    broke up the string declaration to please Betty

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -6,20 +6,29 @@
 char *rot13(char *c)
 {
 	int i, a, rot = 13;
-	char alph[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph[] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0'
+	};
 
 	for (i = 0; *(c + i); i++)
 	{
-			for (a = 0; *(alph + a); a++)
+		for (a = 0; *(alph + a); a++)
+		{
+			if (((*(c + i) >= 'a' && *(c + i) <= 'z')
+				 || (*(c + i) >= 'A' && *(c + i) <= 'Z'))
+				&&  (*(c + i) == *(alph + a)))
 			{
-				if (((*(c + i) >= 'a' && *(c + i) <= 'z')
-					 || (*(c + i) >= 'A' && *(c + i) <= 'Z'))
-					&&  (*(c + i) == *(alph + a)))
-				{
-					*(c + i) = *(alph + a + rot);
-					break;
-				}
+				*(c + i) = *(alph + a + rot);
+				break;
 			}
+		}
 	}
 	return (c);
 }
