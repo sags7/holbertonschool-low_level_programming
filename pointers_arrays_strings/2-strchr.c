b2806fc5d2417f4ec9f895e94185ba768cbc1a37
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-01 16:24:43 -0500

    Changed retVal

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,8 +9,8 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-			if (*(s + i) == c)
-				return (s + i);
+	for (i = 0; *(s + i) >= '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
 	return ('\0');
 }
