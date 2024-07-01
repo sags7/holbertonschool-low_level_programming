42fcc4b2de338ec9b1f1861ba3971c2bcfa9ad76
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-01 16:13:15 -0500

    fix betty style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,10 +8,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned i;
+	unsigned int i;
 
-	for(i = 0; *(s + i); i++)
-		if(*(s + i) == c)
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
 			return (&*(s + i));
-	return NULL;
+	return (NULL);
 }
