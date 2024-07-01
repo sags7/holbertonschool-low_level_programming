f1f9c3d06aef1e62d5ad454794a36ea265e6258a
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-01 16:06:05 -0500

    added _strchr() that returns the address of the first ocurrence of a char in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+/**
+ * _strchr- returns a pointer to the first occurrence of the character c
+ * in the string s, or null if the character isnt found
+ * @s: the string to search for c
+ * @c: the char to look for in s
+ * Return: a pointer to the first occurence of c
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned i;
+
+	for(i = 0; *(s + i); i++)
+		if(*(s + i) == c)
+			return (&*(s + i));
+	return NULL;
+}
