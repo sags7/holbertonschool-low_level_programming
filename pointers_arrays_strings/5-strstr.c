d93722765e77d7773a53ec8f7ceccf11137b1f44
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-01 19:57:49 -0500

    handle special case for empty needle string

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,6 +10,9 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int i, j;
 	char *retVal;
 
+	if (*needle == '\0')
+		return (haystack);
+
 	for (i = 0; *(haystack + i); i++)
 	{
 		if (*(haystack + i) == *needle)
