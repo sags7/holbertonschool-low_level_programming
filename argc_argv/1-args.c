52207d41e92ed8357bd613d14954f7ea54e6d344
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-02 22:38:21 -0500

    add 1-args.c program that prints the amount of arguments passed to it to stdout

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- main program entry point
+ * @argsc: the amount of arguments passed to the program
+ * @argsv: an array containing the args passed to the program
+ * Return: always 0 if successful
+ */
+int main(int argsc, char *argsv[] __attribute__((unused)))
+{
+	printf("%d\n", argsc);
+	return (0);
+}
