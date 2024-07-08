24af2b60cf9363fb3a3e295604faf16a462cefaf
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-08 13:39:54 -0500

    handled malloc return null

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
 	return (arr);
