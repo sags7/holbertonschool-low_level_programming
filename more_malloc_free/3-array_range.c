ac816fb41db3c5757cee84b6acb8fdc1724cdddb
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-11 12:24:05 -0500

    fix off-by-1 at for loop

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -7,16 +7,17 @@
  */
 int *array_range(int min, int max)
 {
-	int *rv = NULL, i = 0;
+	int *rv = NULL, i = 0, size = 0;
 
 	if (min > max)
 		return (NULL);
+	size = max - min + 1;
 
-	rv = malloc((max - min + 1) * sizeof(int));
+	rv = malloc(size * sizeof(int));
 	if (!rv)
 		return (NULL);
 
-	for (i = 0; i <= max - min + 1; i++)
+	for (i = 0; i < size; i++)
 		rv[i] = min + i;
 
 	return (rv);
