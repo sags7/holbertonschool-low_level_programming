0d7a39a23740e9d2c458672337b35ef8f53bf92f
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-11 14:22:18 -0500

    freed ptr after successful realloc()

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -18,6 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		for (i = 0; i > old_size; i++)
 			((char *)rv)[i] = ((char *)ptr)[i];
+		free(ptr);
 		return (rv);
 	}
 
