9cabc940b2fef5d1e40b3504bd81b716d3555e46
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-11 10:54:06 -0500

    array_range(): creates an array containing all the numbers from min to max and rets its pointer

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * array_range- creates an array of integers and returns its pointer
+ * @min: the first value to be included in the array
+ * @max: the last value to be included in the array
+ * Return: the pointer to the created array
+ */
+int *array_range(int min, int max)
+{
+	int *rv = NULL, i = 0;
+
+	if (min > max)
+		return (NULL);
+
+	rv = malloc((max - min + 1) * sizeof(int));
+	if (!rv)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+		rv[i] = i;
+
+	return (rv);
+}
