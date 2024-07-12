16e1f908863e533641bae605e1b1259b8ff9a5fb
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-12 11:26:54 -0500

    added betty doc to struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog struct data type
+ * @name: the dogs name
+ * @age: the dogs name
+ * @owner: the dogs owners name
+ */
 struct dog
 {
 	char *name;
