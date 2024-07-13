20a1097dfcbfdeb3c6c240afd0d72b57a3216964
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-13 15:21:32 -0500

    added free_dog() frees memory allocated by new_dog()

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees the memory allocated by new_dog
+ * @d: the stuct dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
