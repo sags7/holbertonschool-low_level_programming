a545d1feede0b0a80b3314666ef2624395486100
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-16 11:22:33 -0500

    int_index(): searches an int arr for an int and returns the index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+/**
+ * int_index - searches an array for a specific int
+ * @array: the array to search in
+ * @size: size of the array
+ * @cmp: function used to compare values
+ * Return: the first element for which the cmp function doesnt return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (array[i]);
+	return (-1);
+}
