1487bfdfc6e8038462b1a1093bae874c85ab855b
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 14:20:25 -0500

    fixed typo

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i = 0;
 
-	if (n = 0)
+	if (n == 0)
 		return;
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
