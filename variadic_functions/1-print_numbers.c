1f7e1770d6068f476912a46354b3b2c5143d4617
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 11:24:57 -0500

    handled 0 n argument

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,6 +10,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i = 0;
 
+	if (n == 0)
+		return;
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 	{
