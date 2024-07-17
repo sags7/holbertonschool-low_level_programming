28df03e730786addae582f054dab46f8799c8d3c
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 11:16:00 -0500

    handled null separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,7 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d", va_arg(args, int));
-		printf("%s", separator);
+		if (separator)
+			printf("%s", separator);
 	}
 	printf("%d\n", va_arg(args, int));
 	va_end(args);
