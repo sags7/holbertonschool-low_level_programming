a56d402d8d88a0da01c282c1f4663d303c904500
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 14:19:44 -0500

    handled 0 arguments

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,8 +11,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i = 0;
 
+	if (n = 0)
+		return;
 	va_start(args, n);
-
 	for (i = 0; i < n - 1; i++)
 	{
 		char *str = va_arg(args, char *);
