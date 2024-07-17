ae87ed4b05a0ee4cbce72f443600e436e10cc810
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 14:22:29 -0500

    handled o arguments differently

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,11 +12,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 	{
 		char *str = va_arg(args, char *);
+
 		if (str)
 			printf("%s", str);
 		if (separator)
