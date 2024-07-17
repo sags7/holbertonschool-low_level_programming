d2f418d372711638fe5b85a7d395221567748e89
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 14:24:21 -0500

    handled null arguments

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,6 +23,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (str)
 			printf("%s", str);
+		else
+			printf("(nil)");
 		if (separator)
 			printf("%s", separator);
 	}
