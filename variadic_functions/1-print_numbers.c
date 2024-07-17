6d490f21d7f723b48ad0abd66e28f81deba866aa
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 11:13:02 -0500

    print_numbers(): prints all numbers separated by a passed string

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints numbers separated by a passed string
+ * @separator: the string to separate the numbers
+ * @n: the amount of arguments to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	va_start(args, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+	}
+	printf("%d\n", va_arg(args, int));
+	va_end(args);
+}
