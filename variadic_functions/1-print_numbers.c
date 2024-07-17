9e6eee80dd46593f8b2c6235c607a5d5cadd29e9
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-17 11:27:39 -0500

    change how to handle 0n inpuit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,7 +11,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(args, n);
 	for (i = 0; i < n - 1; i++)
 	{
