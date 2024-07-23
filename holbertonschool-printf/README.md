d40e6bae2a0ece6b71ef1e02dd8906e05bccb260
Juan Sebastian Aramburo <103143034+sags7@users.noreply.github.com>
2024-07-23 11:40:44 -0700

    Delete holbertonschool-printf directory and moved to own repo

--- holbertonschool-printf/README.md
+++
@@ -1,2 +0,0 @@
-The _printf project is my own implementation of the standard printf() function.
-It is meant to test my understanding of variadic functions in C.
--- holbertonschool-printf/test_files/handleEscPerc.c
+++
@@ -1,9 +0,0 @@
-/**
- * handleEscPerc - handles escaped %% for _printf
- * @wd: the data structure with the needed data for _printf
- * @flags: optional formatting flags
- */
-void handleEscPerc(workingData *wd, char *flags)
-{
-	wd->outputStr[(*wd->outputPosition)++] = '%';
-}
