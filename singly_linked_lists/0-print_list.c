765a87c93f13b028ac5ee4f64df61d50a21422f6
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-29 17:18:42 -0500

    change _putchar for printf()

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -8,31 +9,42 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	/**int i = 0;*/
 	int retVal = 0;
 	const list_t *index = h;
 
 		index = h;
 		while (index)
 		{
-			_putchar('[');
-			_putchar(index->len + '0');
-			_putchar(']');
-			_putchar(' ');
+			/**
+			*i = 0;
+			*
+			*_putchar('[');
+			*_putchar(index->len + '0');
+			*_putchar(']');
+			*_putchar(' ');
+			*/
+			printf ("[%d] ", index->len);
 
-			i = 0;
 			if (index->str)
-				while (index->str[i])
-					_putchar(index->str[i++]);
-				else
-				{
-					_putchar('(');
-					_putchar('n');
-					_putchar('i');
-					_putchar('l');
-					_putchar(')');
-				}
-			_putchar('\n');
+				printf("%s", index->str);
+				/**
+				 * while (index->str[i])
+				 *	_putchar(index->str[i++]);
+				 */
+			else
+			{
+				printf("(nil)");
+				/**
+				*_putchar('(');
+				*_putchar('n');
+				*_putchar('i');
+				*_putchar('l');
+				*_putchar(')');
+				*/
+			}
+			printf("\n");
+			/**_putchar('\n');*/
 			index = index->next;
 			retVal++;
 		}
