96817a33289aa56df6dbe58a663f66dc03412adb
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-30 20:34:27 -0500

    Fix: Betty for 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newNode;
 		index = *head;
 	}
-	while(index->next)
+	while (index->next)
 		index = index->next;
 
 	index->next = newNode;
