2772315c2d2d8425d94bdaf3460d8248770485e7
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-31 16:42:13 -0500

    Documentation: fix betty for 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,39 +9,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *newNode;
-    dlistint_t *current = *h;
+	dlistint_t *newNode;
+	dlistint_t *current = *h;
 
-    newNode = malloc(sizeof(dlistint_t));
-    if (!newNode)
-        return (NULL);
-    newNode->prev = NULL;
-    newNode->next = NULL;
-    newNode->n = n;
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	newNode->n = n;
 
-  if (idx == 0)
-    {
-        newNode->next = *h;
-        if (*h)
-            (*h)->prev = newNode;
-        *h = newNode;
-        return (newNode);
-    }
+	if (idx == 0)
+	{
+		newNode->next = *h;
+		if (*h)
+			(*h)->prev = newNode;
+		*h = newNode;
+		return (newNode);
+	}
 
-    while (current != NULL && idx-- > 1)
-        current = current->next;
+	while (current != NULL && idx-- > 1)
+		current = current->next;
 
-    if (idx > 0 || current == NULL)
-    {
-        free(newNode);
-        return (NULL);
-    }
+	if (idx > 0 || current == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
 
-    newNode->next = current->next;
-    newNode->prev = current;
-    if (current->next)
-        current->next->prev = newNode;
-    current->next = newNode;
+	newNode->next = current->next;
+	newNode->prev = current;
+	if (current->next)
+		current->next->prev = newNode;
+	current->next = newNode;
 
-    return (newNode);
+	return (newNode);
 }
