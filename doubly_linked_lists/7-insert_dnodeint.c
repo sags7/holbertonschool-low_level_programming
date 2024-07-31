d43f2097425280a2ed7cab9f6ddba68d964483ad
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-31 16:38:36 -0500

    Feature: insert_dnodeint_end()

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index on a list
+ * @h: a pointer to the head pointer
+ * @idx: the index to insert the new node at
+ * @n: the data for the new node
+ * Return: the address to the new node or NULL if fails
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *newNode;
+    dlistint_t *current = *h;
+
+    newNode = malloc(sizeof(dlistint_t));
+    if (!newNode)
+        return (NULL);
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    newNode->n = n;
+
+  if (idx == 0)
+    {
+        newNode->next = *h;
+        if (*h)
+            (*h)->prev = newNode;
+        *h = newNode;
+        return (newNode);
+    }
+
+    while (current != NULL && idx-- > 1)
+        current = current->next;
+
+    if (idx > 0 || current == NULL)
+    {
+        free(newNode);
+        return (NULL);
+    }
+
+    newNode->next = current->next;
+    newNode->prev = current;
+    if (current->next)
+        current->next->prev = newNode;
+    current->next = newNode;
+
+    return (newNode);
+}
