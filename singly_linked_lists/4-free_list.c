da16c8bf62d04eabc50b89ac79b74462f0352108
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-30 20:53:03 -0500

    Feature: free_list() frees all malloced data of a list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees the malloced space for a list_t
+ * @head: a pointer to the list
+ */
+void free_list(list_t *head)
+{
+    list_t *next;
+
+    while (head)
+    {
+        next = head->next;
+        free(head->str);
+        free(head);
+		head = next;
+    }
+}
