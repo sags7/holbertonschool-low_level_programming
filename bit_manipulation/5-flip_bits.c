256cad6b88b9f42e8c0ed73834bd31764e896cd6
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-08-01 15:05:03 -0500

    Feature: flip_bits() returns the amount of different bits between n and m

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits that need
+ * to be flipped in order to change a number to another
+ * @n: first number to compare
+ * @m: second number to compare
+ * Return: the numer of different bits between the two numbers
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = sizeof(unsigned long int) * 8 - 1;
+	unsigned long int mask = 1;
+	unsigned int retVal = 0;
+
+	while (i > 0)
+	{
+		if ((mask & n) ^ (mask & m))
+			retVal++;
+		mask <<= 1;
+		i--;
+	}
+	return (retVal);
+}
