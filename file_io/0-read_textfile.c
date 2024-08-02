6fc30df42534c431766c231649ae0de2229db1d1
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-08-02 11:30:06 -0500

    Fix: fix not returning 0 when filename is NULL

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,13 +13,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t readSize = 0;
 	char *readBuffer;
-	int fd = open(filename, O_RDONLY);
-
-	if (fd < 0)
-		return (-1);
+	int fd;
 
 	if (!filename)
 		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (-1);
 
 	readBuffer = malloc(sizeof(char) * letters);
 	if (!readBuffer)
